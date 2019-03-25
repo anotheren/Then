7e36afb97168e126c3ad6f49cd157f85e9c0e431
liudong <liudong.edward@gmail.com>
2019-03-25 15:07:17 +0800

    add Carthage support

---
+++ Sources/Then.h
@@ -0,0 +1,19 @@
+//
+//  Then.h
+//  Then
+//
+//  Created by 刘栋 on 2019/3/25.
+//  Copyright © 2019 anotheren.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Then.
+FOUNDATION_EXPORT double ThenVersionNumber;
+
+//! Project version string for Then.
+FOUNDATION_EXPORT const unsigned char ThenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Then/PublicHeader.h>
+
+
